42130e9082fdfaebf700abe02fed34316d7f7592
EmmanuelVegaLira <emmanuelvegalira@gmail.com>
2023-02-15 15:29:17 -0600

    begin:init commit

--- src/models/model.notification.ts
+++ src/models/model.notification.ts
@@ -2,13 +2,11 @@ import mongoose, {Schema} from 'mongoose';
 
 const OfferNotificationSchema: Schema = new Schema({
   notification_type: { type: String, required: true, default: 'offer'},
-  sender_id: { type: String, required: true},
-  recipient_id: { type: String, required: true},
+  store_id: { type: String, required: true},
+  reading_users: { type: Array, required: true},
   offer_id: { type: String, required: true},
   message: { type: String, required: true},
-  date_create: { type: Date, default: Date.now},
-  date_read: { type: Date},
-  is_bidirectional: { type: Boolean, default: true}
+  date_create: { type: Date, default: Date.now}
 }, { collection: 'notificacion'});
 
 export default mongoose.model('Notificacion', OfferNotificationSchema);
